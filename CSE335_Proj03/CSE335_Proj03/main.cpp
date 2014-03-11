c25f44fca9fd02200897f840d04375bfd0a2b5c4
njd911 <njdhockeydude@gmail.com>
2014-03-11 14:35:50 -0400

    created project

---
+++ CSE335_Proj03/CSE335_Proj03/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CSE335_Proj03
+//
+//  Created by Nick Durak on 3/11/14.
+//  Copyright (c) 2014 Nick Durak. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
